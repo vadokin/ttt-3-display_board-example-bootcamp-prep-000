86cc2853e82282428371b80ab3ea7b402ec568a6
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-21 16:56:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,17 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   puts "A Tic Tac Toe Board"
-  # cell = "   "
-  # row = [cell, "|", cell, "|", cell]
-  # line = 11.times {print "-"}
 
   puts "   " + "|" + "   " + "|" + "   "
   puts "-----------"
-  # puts 11.times {print "-"}
 
   puts "   " + "|" + "   " + "|" + "   "
   puts "-----------"
-  # puts 11.times {print "-"}
 
   puts "   " + "|" + "   " + "|" + "   "
 
