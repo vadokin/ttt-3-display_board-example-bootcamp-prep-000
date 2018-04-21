72d69cf41e2cdf1b125b0e4456c64c349e0ecbdf
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-21 16:45:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,11 +3,7 @@ def display_board
   puts "A Tic Tac Toe Board"
   row = ["   ", "|", "   ", "|", "   "]
   line = Array.new(11, "-")
-  puts row
-  puts line
-  puts row
-  puts line
-  puts row
+  
 end
 
 display_board
